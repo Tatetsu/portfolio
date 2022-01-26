a70301ecc029930c54b71b37f74c2014ee2dbfeb
masataka <masataka@masatakanoMacBook-Air.local>
2022-01-26 14:34:48 +0900

    更新しました

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/firebase.js'
+    '~/plugins/firebase.js',
+    { src: '@/plugins/swiper', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -57,5 +58,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    vender: [
+      'vue-awesome-swiper'
+    ]
   }
 }
--- pages/playlist.vue
+++ pages/playlist.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-        おそらくMotion.vueの内容がここに入るはず
+  <div>
+    <div class="question_number">
+      <p class="leading-loose text-3xl">診断結果</p>
+      <p class="leading-loose text-lg">RECOMMEND FOR YOU</p>
     </div>
+    <h2 class="question_title text-3xl">
+      あなたにカスタマイズされた<br />おすすめのトレーニングメニュー
+    </h2>
+    <div class="result border-2 bg-white py-5 px-10 flex">
+        <div class="result_left w-1/3 p-3 relative">
+      <div class="result_img">
+          <img src="../assets/img/AdobeStock_250293315.jpeg" alt="">
+      </div>
+      <div class="result_number absolute top-10 left-10">
+        <p class="text-3xl font-bold">01</p>
+      </div>
+        </div>
+        <div class="result_right w-2/3">
+            <h2 class="title">有酸素運動</h2>
+            <p>aerobic exercise</p>
+            <div class="result_right_inner px-5">
+                <p>ここにはテキストを入れますここにはテキストを入れますここにはテキストを入れますここにはテキストを入れます
+                    ここにはテキストを入れますここにはテキストを入れますここにはテキストを入れますここにはテキストを入れます
+                    ここにはテキストを入れますここにはテキストを入れますここにはテキストを入れますここにはテキストを入れます
+                    ここにはテキストを入れますここにはテキストを入れますここにはテキストを入れますここにはテキストを入れます
+                </p>
+            </div>
+        </div>
+    </div>
+  </div>
 </template>
 
-<script>
+<script></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
