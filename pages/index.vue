e553bb581f7fe5ecd033a74dffffd5c6794a9cea
masataka <masataka@masatakanoMacBook-Air.local>
2022-02-11 19:03:42 +0900

    APIの取得方法について理解できてきた

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <!-- <p>{{ contents[0].image.url }}</p>
+    <div v-for="content in contents" :key="content">
+      <p>{{ content.id }}</p>
+      <p><img :src="content.img.url" alt=""> </p>
+    </div> -->
     <div class="new flex flex-wrap items-center sm:flex-row flex-col p-5">
       <div class="card my-5 px-3">
         <div class="thumbnail">
@@ -76,35 +81,53 @@
     </div>
     <div class="playlist py-5">
       <h2 class="pt-3">おすすめのプレイリスト</h2>
-      <div class="playlist_inner flex items-center sm:flex-row flex-col pt-3">
-        <div class="playlist_inner_get mx-4 my-5">
-          <router-link to="/">
-            <img
-              src="../assets/img/AdobeStock_153482564.jpeg"
-              alt=""
-              class="rounded-lg"
-            />
-          </router-link>
+      
+      <!-- <div
+        class="playlist_inner flex items-center sm:flex-row flex-col pt-3"
+        v-for="content in contents"
+        :key="content"
+      >
+        <div v-for="image in content.image" :key="image">
+          <div class="playlist_inner_get mx-4 my-5">
+            <router-link to="/">
+              <img 
+              :src="image.url" 
+              alt="" 
+              class="rounded-lg" />
+            </router-link>
+          </div>
         </div>
-        <div class="playlist_inner_get mx-4 my-5">
-          <router-link to="/">
-            <img
-              src="../assets/img/AdobeStock_153482564.jpeg"
-              alt=""
-              class="rounded-lg"
-            />
-          </router-link>
+      </div> -->
+      <div
+        class="playlist_inner flex items-center sm:flex-row flex-col pt-3"
+      >
+
+          <div class="playlist_inner_get mx-4 my-5">
+            <router-link to="/">
+              <img 
+              :src="contents[0].image.url" 
+              alt="" 
+              class="rounded-lg" />
+            </router-link>
+          </div>
+          <div class="playlist_inner_get mx-4 my-5">
+            <router-link to="/">
+              <img 
+              :src="contents[0].image.url" 
+              alt="" 
+              class="rounded-lg" />
+            </router-link>
+          </div>
+          <div class="playlist_inner_get mx-4 my-5">
+            <router-link to="/">
+              <img 
+              :src="contents[0].image.url" 
+              alt="" 
+              class="rounded-lg" />
+            </router-link>
+          </div>
+          <!-- ここはv-forで回す -->
         </div>
-        <div class="playlist_inner_get mx-4 my-5">
-          <router-link to="/">
-            <img
-              src="../assets/img/AdobeStock_153482564.jpeg"
-              alt=""
-              class="rounded-lg"
-            />
-          </router-link>
-        </div>
-      </div>
     </div>
     <div class="blog py-5">
       <h2 class="pt-3">搭載した機能をブログブログ（Qiita）で書きます</h2>
@@ -144,12 +167,24 @@
 <script>
 export default {
   layout: "",
+  async asyncData({ query, $microcms }) {
+    const id = query.id;
+    console.log(id);
+    const { contents } = await $microcms.get({
+      endpoint: "motion",
+      contentId: id,
+    });
+    console.log(contents);
+    return {
+      contents,
+    };
+  },
 };
 </script>
 
 <style scoped>
 img {
-  width: 100%;
+  /* width: 100%; */
 }
 .card {
   width: calc(100% / 1);
@@ -163,9 +198,8 @@ img {
   }
 }
 @media (min-width: 768px) {
-
 }
-  @media (min-width: 1024px) {
+@media (min-width: 1024px) {
   .card {
     width: calc(100% / 3);
   }
--- pages/result.vue
+++ pages/result.vue
@@ -7,27 +7,10 @@
         あなたにカスタマイズされた<br />おすすめのトレーニングメニュー
       </h2>
     </div>
-    <p>{{ contents.id }}</p>
-
-    <div v-for="content in contents" :key="content">
-      <img :src="content.image.url" class="w-80 shadow-lg" />
-      <div v-for="movie in content.movies" :key="movie">
-        <h3 class="text-center font-bold mb-5">{{ movie.fieldId }}</h3>
-        <!-- <iframe
-          :src="movie.url"
-          width="100%"
-          height="100%"
-          frameborder="0"
-        ></iframe> -->
-      </div>
-    </div>
-
     <div
-      class="result border-2 bg-white py-5 px-10 flex"
-      v-for="index in 6"
-      :key="index"
+      class="result border-2 bg-white py-5 px-10 flex flex-col xl:flex-row items-center"
     >
-      <div class="result_left w-1/3 p-3 relative">
+      <div class="result_left lg:w-2/3 xl:w-1/3 p-3 relative">
         <div class="result_img">
           <img src="../assets/img/AdobeStock_250293315.jpeg" alt="" />
           <!-- <img :src="content.image.url" class="w-80 shadow-lg" /> -->
@@ -36,9 +19,11 @@
           <p class="text-3xl font-bold">01</p>
         </div>
       </div>
-      <div class="result_right w-2/3">
+      <div class="result_right xl:w-2/3">
         <h2 class="title">有酸素運動</h2>
+        <!-- ここにはMicroCMSから取得したタイトルを入れます -->
         <p>aerobic exercise</p>
+        <!-- 取得したタイトルを英語変換 -->
         <div class="result_right_inner px-5">
           <p>
             ここにはテキストを入れますここにはテキストを入れますここにはテキストを入れますここにはテキストを入れます
@@ -46,9 +31,70 @@
             ここにはテキストを入れますここにはテキストを入れますここにはテキストを入れますここにはテキストを入れます
             ここにはテキストを入れますここにはテキストを入れますここにはテキストを入れますここにはテキストを入れます
           </p>
+          <!-- ここには説明を取得する -->
         </div>
       </div>
     </div>
+    <div
+      class="result_video border-2 bg-white py-5 px-10 flex items-center flex-wrap flex-row"
+    >
+      <div class="result_video_inner px-2 my-3 sm:w-1/2 md:w-1/3">
+        <h3 class="text-md md:text-lg">
+          ここにはMicroCMSから取得した動画のタイトルを入れます
+        </h3>
+        <img src="../assets/img/AdobeStock_153482564.jpeg" alt="" />
+        <!-- ここにはMicroCMSから取得した動画を入れます -->
+      </div>
+      <div class="result_video_inner px-2 my-3 sm:w-1/2 md:w-1/3">
+        <h3 class="text-md md:text-lg">
+          ここにはMicroCMSから取得した動画のタイトルを入れます
+        </h3>
+        <img src="../assets/img/AdobeStock_153482564.jpeg" alt="" />
+        <!-- ここにはMicroCMSから取得した動画を入れます -->
+      </div>
+      <div class="result_video_inner px-2 my-3 sm:w-1/2 md:w-1/3">
+        <h3 class="text-md md:text-lg">
+          ここにはMicroCMSから取得した動画のタイトルを入れます
+        </h3>
+        <img src="../assets/img/AdobeStock_153482564.jpeg" alt="" />
+        <!-- ここにはMicroCMSから取得した動画を入れます -->
+      </div>
+      <div class="result_video_inner px-2 my-3 sm:w-1/2 md:w-1/3">
+        <h3 class="text-md md:text-lg">
+          ここにはMicroCMSから取得した動画のタイトルを入れます
+        </h3>
+        <img src="../assets/img/AdobeStock_153482564.jpeg" alt="" />
+        <!-- ここにはMicroCMSから取得した動画を入れます -->
+      </div>
+      <div class="result_video_inner px-2 my-3 sm:w-1/2 md:w-1/3">
+        <h3 class="text-md md:text-lg">
+          ここにはMicroCMSから取得した動画のタイトルを入れます
+        </h3>
+        <img src="../assets/img/AdobeStock_153482564.jpeg" alt="" />
+        <!-- ここにはMicroCMSから取得した動画を入れます -->
+      </div>
+      <div class="result_video_inner px-2 my-3 sm:w-1/2 md:w-1/3">
+        <h3 class="text-md md:text-lg">
+          ここにはMicroCMSから取得した動画のタイトルを入れます
+        </h3>
+        <img src="../assets/img/AdobeStock_153482564.jpeg" alt="" />
+        <!-- ここにはMicroCMSから取得した動画を入れます -->
+      </div>
+      <div class="result_video_inner px-2 my-3 sm:w-1/2 md:w-1/3">
+        <h3 class="text-md md:text-lg">
+          ここにはMicroCMSから取得した動画のタイトルを入れます
+        </h3>
+        <img src="../assets/img/AdobeStock_153482564.jpeg" alt="" />
+        <!-- ここにはMicroCMSから取得した動画を入れます -->
+      </div>
+    </div>
+
+    <div v-for="content in contents.contents" :key="content">
+      <div v-for="(movie, index) in content.movies" :key="index">
+        <p>{{ movie.title }}</p>
+      </div>
+    </div>
+    <p>{{ contents.contents[0].movies[0].title }}</p>
   </div>
 </template>
 
@@ -58,7 +104,7 @@ export default {
     const id = query.id;
     // 動画リストのIDを取得する
     console.log(id);
-    const { contents } = await $microcms.get({
+    const contents = await $microcms.get({
       endpoint: "motion",
       contentId: id,
     });
@@ -67,16 +113,13 @@ export default {
       contents,
     };
   },
-
   data() {
     return {
       contents: "",
     };
   },
-
   methods: {},
   computed: {},
-
   mounted: function () {},
 };
 </script>
